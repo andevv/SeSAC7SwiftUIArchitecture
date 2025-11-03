0b6bb9fd7bc868b59f922f7570fe0f5dfc2511c0
andev <andh1006@naver.com>
2025-11-03 10:10:35 +0900

    Initial Commit

---
+++ SeSAC7SwiftUIArchitecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSAC7SwiftUIArchitecture
+//
+//  Created by andev on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
