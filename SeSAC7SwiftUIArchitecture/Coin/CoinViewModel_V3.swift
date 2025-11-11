720fae2968242455f4dec0bb89ae5a19b0aa9746
andev <andh1006@naver.com>
2025-11-11 14:57:03 +0900

    MVVM + IO Pattern 고도화

--- SeSAC7SwiftUIArchitecture/Coin/CoinViewModel_V3.swift
+++ SeSAC7SwiftUIArchitecture/Coin/CoinViewModel_V3.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import Combine
+
+class CoinViewModel_v3: ObservableObject {
+    
+    private var cancellables = Set<AnyCancellable>() //disposeBag과 같음
+    
+    private var input = Input()
+    
+    @Published
+    var output = Output()
+    
+    init() {
+        transform()
+    }
+}
+
+extension CoinViewModel_v3 {
+    
+    struct Input {
+        let viewOnTask = PassthroughSubject<Void, Never>()
+    }
+    
+    struct Output {
+        var market: [Market] = []
+    }
+    
+    enum Action {
+        case viewOnTask
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .viewOnTask:
+            input.viewOnTask.send(())
+        }
+    }
+    
+    private func transform() {
+        input.viewOnTask
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                
+                self.fetchMarket()
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func fetchMarket() {
+        UpbitAPI.fetchAllMarket { [weak self] data in
+            guard let self = self else { return }
+            self.output.market = data
+        }
+    }
+    
+}
--- SeSAC7SwiftUIArchitecture/Coin/CoinView_v3.swift
+++ SeSAC7SwiftUIArchitecture/Coin/CoinView_v3.swift
@@ -5,4 +5,51 @@
 //  Created by andev on 11/11/25.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+struct CoinView_v3: View {
+    
+    @StateObject var viewModel = CoinViewModel_v3()
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                listView()
+            }
+            .navigationTitle("My Money")
+        }
+        .task {
+            viewModel.action(.viewOnTask)
+        }
+    }
+    
+    func listView() -> some View {
+        LazyVStack {
+            ForEach(viewModel.output.market, id: \.self) { item in
+                rowView(item)
+            }
+        }
+    }
+    
+    func rowView(_ item: Market) -> some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.koreanName)
+                    .fontWeight(.bold)
+                Text(item.market)
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            Spacer()
+            Text(item.englishName)
+        }
+        .padding(.horizontal, 20)
+        .padding(.vertical, 6)
+    }
+
+}
+
+#Preview {
+    CoinView_v3()
+}
