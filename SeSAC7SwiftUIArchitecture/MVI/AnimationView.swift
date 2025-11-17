0f3707a68813e5a96eee856a141141b1482f3c5c
andev <andh1006@naver.com>
2025-11-17 11:11:15 +0900

    카드 애니메이션 추가

--- SeSAC7SwiftUIArchitecture/MVI/AnimationView.swift
+++ SeSAC7SwiftUIArchitecture/MVI/AnimationView.swift
@@ -7,9 +7,34 @@
 
 import SwiftUI
 
+struct CardModel: Hashable {
+    let color = Color.random()
+    let name: String
+    let number: Int
+}
+
+extension Color {
+    static func random() -> Color {
+        return Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
+    }
+}
+
 struct AnimationView: View {
     
     @State private var isAnimated = false
+    @State private var isExpandable = false
+    
+    @State private var currentCard = CardModel(name: "", number: 0)
+    
+    @Namespace var animation
+    
+    var cardlist = [
+        CardModel(name: "신한카드", number: 0),
+        CardModel(name: "국민카드", number: 1),
+        CardModel(name: "대구카드", number: 2),
+        CardModel(name: "현대카드", number: 3),
+        CardModel(name: "카카오카드", number: 4)
+    ]
     
     var body: some View {
         VStack {
@@ -26,13 +51,18 @@ struct AnimationView: View {
                 }
             }
         }
+        .overlay {
+            if isAnimated {
+                DetailAnimationView(showDetail: $isAnimated, animation: animation, currentCard: currentCard)
+            }
+        }
     }
     
     func topTitle() -> some View {
         Text("Hello World")
             .font(.largeTitle)
             .bold()
-            .frame(maxWidth: .infinity, alignment: isAnimated ? .leading : .center)
+            .frame(maxWidth: .infinity, alignment: isExpandable ? .leading : .center)
             .overlay(alignment: .trailing) {
                 topOverlayButton()
             }
@@ -43,7 +73,7 @@ struct AnimationView: View {
     func topOverlayButton() -> some View {
         Button {
             withAnimation {
-                isAnimated = false
+                isExpandable = false
             }
         } label: {
             Image(systemName: "plus")
@@ -51,15 +81,69 @@ struct AnimationView: View {
                 .padding()
                 .background(.black, in: Circle())
         }
-        .rotationEffect(.degrees(isAnimated ? 225 : 45))
-        .opacity(isAnimated ? 1 : 0)
+        .rotationEffect(.degrees(isExpandable ? 225 : 45))
+        .opacity(isExpandable ? 1 : 0)
     }
     
     func cardView() -> some View {
         ScrollView {
-            ForEach(0..<5) { item in
+            ForEach(cardlist, id: \.self) { item in
+                oneCard(item)
+            }
+        }
+        .overlay {
+            Rectangle()
+                .fill(.black.opacity(isExpandable ? 0 : 0.01))
+                .onTapGesture {
+                    withAnimation {
+                        isExpandable = true
+                    }
+                }
+        }
+    }
+    
+    func oneCard(_ card: CardModel) -> some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(card.color)
+            .frame(height: 150)
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+            .offset(y: CGFloat(card.number) * (isExpandable ? 0 : -100 ))
+            .matchedGeometryEffect(id: card, in: animation)
+            .onTapGesture {
+                currentCard = card
+                withAnimation {
+                    isAnimated = true
+                }
+            }
+    }
+}
+
+struct DetailAnimationView: View {
+    
+    @Binding var showDetail: Bool
+    
+    let animation: Namespace.ID
+    
+    let currentCard: CardModel
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            VStack {
+                Text(currentCard.name)
                 RoundedRectangle(cornerRadius: 25)
+                    .fill(currentCard.color)
                     .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal)
+                    .matchedGeometryEffect(id: currentCard, in: animation)
+                    .onTapGesture {
+                        withAnimation {
+                            showDetail = false
+                        }
+                    }
             }
         }
     }
