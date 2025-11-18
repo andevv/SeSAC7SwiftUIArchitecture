3fe19e1b24650716cb221ff3ceb728615bba33eb
andev <andh1006@naver.com>
2025-11-18 11:14:19 +0900

    TCA 임시

--- SeSAC7SwiftUIArchitecture/TCA/NumberView.swift
+++ SeSAC7SwiftUIArchitecture/TCA/NumberView.swift
@@ -9,41 +9,52 @@ import SwiftUI
 import Combine
 import ComposableArchitecture
 
-struct NumberState {
-    var number = 0
-}
-
-enum NumberIntent {
-    case increment
-    case decrement
-}
-
-final class NumberStore: ObservableObject {
-    @Published private(set) var state = NumberState()
+/// State, Intent, Store
+/// Reducer Protocol -> State, Action, func reduce
+@Reducer
+struct NumberFeature {
     
-    func action(_ intent: NumberIntent) {
-        switch intent {
-        case .increment:
-            state.number += 1
-        case .decrement:
-            state.number -= 1
+    @ObservableState //State 구조체를 SwiftUI가 관찰하고, 뷰가 업데이트 되도록
+    struct State {
+        var number = 0
+    }
+
+    enum Action {
+        case increment
+        case decrement
+    }
+    
+    // state, action -> Effect
+    // 동기 -> 사이드이펙트 X -> 일관된 결과 보장 (return .none)
+    // 비동기 -> 사이드이펙트 O -> 일관된 결과 보장 X
+    var body: some ReducerOF<Self> {
+        Reduce { state, action in
+            switch action {
+            case .increment:
+                state.number += 1
+                return .none
+            case .decrement:
+                state.number -= 1
+                return .none
+            }
         }
     }
 }
 
+
 struct NumberView: View {
     
-    @State private var store = NumberStore()
+    let store: StoreOf<NumberFeature>
     
     var body: some View {
         VStack {
             Text("number: \(store.state.number)")
             HStack {
                 Button("-") {
-                    store.action(.decrement)
+                    store.send(.decrement)
                 }
                 Button("+") {
-                    store.action(.increment)
+                    store.send(.increment)
                 }
             }
         }
@@ -51,5 +62,7 @@ struct NumberView: View {
 }
 
 #Preview {
-    NumberView()
+    NumberView(store: Store(initialState: NumberFeature.State(), reducer: {
+        NumberFeature()
+    }))
 }
